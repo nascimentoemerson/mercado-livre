c447631138906a355068e8b7f3c8644cdbd19458
nascimentoemerson <contatovemarketing@gmail.com>
2023-06-03 16:20:41 -0300

    feat: implement product card and list

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./product-list.component.sass']
 })
 export class ProductListComponent {
-
+  products = ["Embreagem", "Kit Embreagem", "Coxim Amortecedor" ]
 }
